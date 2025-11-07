31375f8687af6deaadacfca8a60996eb7b959447
Manish Devrani <manishdevrani777@gmail.com>
2025-11-07 12:34:15 +0530

    feat: drag event on drawer

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -73,7 +73,7 @@ export function DrawerContent({
       {isOpen ? (
         <>
           <motion.div
-            className="fixed inset-0 bg-black/40 backdrop-blur-sm"
+            className="fixed inset-0 bg-black/30 backdrop-blur-xs"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
@@ -81,14 +81,23 @@ export function DrawerContent({
           />
           <motion.div
             className={cn(
-              'fixed bottom-0 left-0 right-0 w-11/12 mx-auto bg-card rounded-2xl p-6 z-10',
+              'fixed bottom-0 left-0 right-0 w-[93%] mx-auto bg-card rounded-2xl px-6 pt-2 pb-6 z-10',
               className,
             )}
             initial={{ y: '100%' }}
             animate={{ y: 0 }}
             exit={{ y: '100%' }}
-            transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+            transition={{
+              duration: 0.3,
+              ease: [0.79, 0.14, 0.15, 0.86],
+            }}
+            drag="y"
+            dragConstraints={{ top: 0, bottom: 0 }}
+            onDragEnd={(_, info) => {
+              if (info.offset.y > 120) setIsOpen(false);
+            }}
           >
+            <div className="w-16 h-1 bg-secondary rounded-full mx-auto mb-4" />
             {children}
           </motion.div>
         </>
